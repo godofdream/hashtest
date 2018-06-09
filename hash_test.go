b41fd9c115dc4963ff64d40b120113532eeb6308
godofdream <soilfiction@gmail.com>
2018-06-09 17:40:25 +0200

    added more benchmarks

--- hash_test.go
+++ hash_test.go
@@ -10,6 +10,8 @@ import (
 	//
 	//"hash/adler32"
 	// "hash/crc32"
+	"crypto/sha256"
+	"fmt"
 	"hash/crc32"
 	"hash/crc64"
 	"hash/fnv"
@@ -57,209 +59,189 @@ import (
 	// "crypto/md5"
 	// "crypto/rand"
 	// "crypto/sha1"
-	// "crypto/sha256"
 	// "crypto/sha512"
 	"github.com/minio/highwayhash"
 	//sha256Avx512 "github.com/minio/sha256-simd"
-	//"golang.org/x/crypto/blake2b"
+	"golang.org/x/crypto/blake2b"
 )
 
 // 32 bit hashes
 
-var h32djb = func(k []byte) uint32 {
-	h := dgohash.NewDjb32()
-	h.Write(k)
-	return h.Sum32()
+func Benchmark32bitDJB(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 {
+		h := dgohash.NewDjb32()
+		h.Write(k)
+		return h.Sum32()
+	})
 }
 
-func Benchmark32DJB(b *testing.B) { benchmarkHash32(b, "djb", h32djb) }
-
-var h32java = func(k []byte) uint32 {
-	h := dgohash.NewJava32()
-	h.Write(k)
-	return h.Sum32()
+func Benchmark32bitJava(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 {
+		h := dgohash.NewJava32()
+		h.Write(k)
+		return h.Sum32()
+	})
 }
 
-func Benchmark32Java(b *testing.B) { benchmarkHash32(b, "java", h32java) }
-
-var h32elf = func(k []byte) uint32 {
-	h := dgohash.NewElf32()
-	h.Write(k)
-	return h.Sum32()
+func Benchmark32bitELF(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 {
+		h := dgohash.NewElf32()
+		h.Write(k)
+		return h.Sum32()
+	})
 }
 
-func Benchmark32ELF(b *testing.B) { benchmarkHash32(b, "djb", h32elf) }
-
-var h32sdbm = func(k []byte) uint32 {
-	h := dgohash.NewSDBM32()
-	h.Write(k)
-	return h.Sum32()
+func Benchmark32bitSDBM(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 {
+		h := dgohash.NewSDBM32()
+		h.Write(k)
+		return h.Sum32()
+	})
 }
 
-func Benchmark32SDBM(b *testing.B) { benchmarkHash32(b, "sdbm", h32sdbm) }
-
-var h32sqlite = func(k []byte) uint32 {
-	h := dgohash.NewSQLite32()
-	h.Write(k)
-	return h.Sum32()
+func Benchmark32bitSQLITE(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 {
+		h := dgohash.NewSQLite32()
+		h.Write(k)
+		return h.Sum32()
+	})
 }
 
-func Benchmark32SQLITE(b *testing.B) { benchmarkHash32(b, "sqlite", h32sqlite) }
-
-var h32jenkins = func(k []byte) uint32 {
-	h := dgohash.NewJenkins32()
-	h.Write(k)
-	return h.Sum32()
+func Benchmark32bitJenkins(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 {
+		h := dgohash.NewJenkins32()
+		h.Write(k)
+		return h.Sum32()
+	})
 }
 
-func Benchmark32Jenkins(b *testing.B) { benchmarkHash32(b, "jenkins", h32jenkins) }
+func Benchmark32bitCRC32(b *testing.B) { benchmarkHash(b, crc32.ChecksumIEEE) }
+func Benchmark32bitMarvin(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 { return marvin32.Sum32(0, k) })
+}
 
-var h32crc32 = func(k []byte) uint32 { return crc32.ChecksumIEEE(k) }
+func Benchmark32bitMurMur2(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 { return murmur2.MurmurHash2(k, 0) })
+}
+func Benchmark32bitMurMur2a(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 { return murmur2.MurmurHash2A(k, 0) })
+}
 
-func Benchmark32CRC32(b *testing.B) { benchmarkHash32(b, "crc32", h32crc32) }
+func Benchmark32bitcityhash(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint32 { return cityhash.CityHash32(k, uint32(len(k))) })
+}
 
-var h32marvin = func(k []byte) uint32 { return marvin32.Sum32(0, k) }
+func Benchmark32bitSpooky(b *testing.B) { benchmarkHash(b, spooky.Hash32) }
 
-func Benchmark32Marvin(b *testing.B) { benchmarkHash32(b, "Marvin32", h32marvin) }
+func Benchmark32bitFarm(b *testing.B) { benchmarkHash(b, farm.Hash32) }
 
-var h32murmur3 = func(k []byte) uint32 { return murmur3.Sum32(k) }
+func Benchmark32bitXXHash(b *testing.B) { benchmarkHash(b, xxhash.Checksum32) }
 
-func Benchmark32MurMur3(b *testing.B) { benchmarkHash32(b, "murmur3", h32murmur3) }
+// murmur3 "github.com/spaolacci/murmur3"
+func Benchmark32bitMurMur3(b *testing.B)  { benchmarkHash(b, murmur3.Sum32) }
+func Benchmark64bitMurMur3(b *testing.B)  { benchmarkHash(b, murmur3.Sum64) }
+func Benchmark128bitMurMur3(b *testing.B) { benchmarkHash(b, murmur3.Sum128) }
 
-var h32murmur2 = func(k []byte) uint32 { return murmur2.MurmurHash2(k, 0) }
+func Benchmark64bitMurMur2(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return murmur2.MurmurHash64A(k, 0) })
+}
 
-func Benchmark32MurMur2(b *testing.B) { benchmarkHash32(b, "murmur2", h32murmur2) }
+func Benchmark64bitSpooky(b *testing.B) { benchmarkHash(b, spooky.Hash64) }
 
-var h32murmur2a = func(k []byte) uint32 { return murmur2.MurmurHash2A(k, 0) }
+func Benchmark64bitSipHash(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return dchestsip.Hash(0, 0, k) })
+}
 
-func Benchmark32MurMur2a(b *testing.B) { benchmarkHash32(b, "murmur2a", h32murmur2a) }
+func Benchmark64bitFarm(b *testing.B) { benchmarkHash(b, farm.Hash64) }
 
-var h32cityhash = func(k []byte) uint32 { return cityhash.CityHash32(k, uint32(len(k))) }
+func Benchmark64bitCity(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return cityhash.CityHash64(k, uint32(len(k))) })
+}
 
-func Benchmark32cityhash(b *testing.B) { benchmarkHash32(b, "cityhash", h32cityhash) }
+func Benchmark64bitMetro(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return metro.Hash64(k, 0) })
+}
 
-var h32spooky = func(k []byte) uint32 { return spooky.Hash32(k) }
+func Benchmark64bitXXHash(b *testing.B) { benchmarkHash(b, xxhash.Checksum64) }
 
-func Benchmark32Spooky(b *testing.B) { benchmarkHash32(b, "Spooky", h32spooky) }
+func Benchmark64bitXXFast(b *testing.B) { benchmarkHash(b, xxhashfast.Sum64) }
 
-var h32farm = func(k []byte) uint32 { return farm.Hash32(k) }
+func Benchmark64bitFasthash(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return fasthash.Hash64(0, k) })
+}
 
-func Benchmark32Farm(b *testing.B) { benchmarkHash32(b, "Farm", h32farm) }
-
-var h32xxhash = func(k []byte) uint32 { return xxhash.Checksum32(k) }
-
-func Benchmark32XXHash(b *testing.B) { benchmarkHash32(b, "XXHash", h32xxhash) }
-
-var h32farmhash = func(k []byte) uint32 { return farmhash.Hash32(k) }
-
-func Benchmark32FarmHash(b *testing.B) { benchmarkHash32(b, "Farmhash", h32farmhash) }
-
-// 64 bit hashes
-
-var h64murmur3 = func(k []byte) uint64 { return murmur3.Sum64(k) }
-
-func Benchmark64MurMur3(b *testing.B) { benchmarkHash64(b, "murmur3", h64murmur3) }
-
-var h64murmur2 = func(k []byte) uint64 { return murmur2.MurmurHash64A(k, 0) }
-
-func Benchmark64MurMur2(b *testing.B) { benchmarkHash64(b, "murmur2", h64murmur2) }
-
-var h64spooky = func(k []byte) uint64 { return spooky.Hash64(k) }
-
-func Benchmark64Spooky(b *testing.B) { benchmarkHash64(b, "Spooky", h64spooky) }
-
-var h64siphash = func(k []byte) uint64 { return dchestsip.Hash(0, 0, k) }
-
-func Benchmark64SipHash(b *testing.B) { benchmarkHash64(b, "SipHash", h64siphash) }
-
-var h64farm = func(k []byte) uint64 { return farm.Hash64(k) }
-
-func Benchmark64Farm(b *testing.B) { benchmarkHash64(b, "Farm", h64farm) }
-
-var h64city = func(k []byte) uint64 { return cityhash.CityHash64(k, uint32(len(k))) }
-
-func Benchmark64City(b *testing.B) { benchmarkHash64(b, "City", h64city) }
-
-var h64metro = func(k []byte) uint64 { return metro.Hash64(k, 0) }
-
-func Benchmark64Metro(b *testing.B) { benchmarkHash64(b, "Metro", h64metro) }
-
-var h64xxhash = func(k []byte) uint64 { return xxhash.Checksum64(k) }
-
-func Benchmark64XXHash(b *testing.B) { benchmarkHash64(b, "XXHash", h64xxhash) }
-
-var h64xxhashfast = func(k []byte) uint64 { return xxhashfast.Sum64(k) }
-
-func Benchmark64XXFast(b *testing.B) { benchmarkHash64(b, "XXFast", h64xxhashfast) }
-
-var fsthash = func(k []byte) uint64 { return fasthash.Hash64(0, k) }
-
-func Benchmark64Fasthash(b *testing.B) { benchmarkHash64(b, "Fasthash", fsthash) }
-
-var h64igh = func(k []byte) uint64 { return highway.Hash(highway.Lanes{}, k) }
-
-func Benchmark64Highway(b *testing.B) { benchmarkHash64(b, "Highway", h64igh) }
+func Benchmark64bitHighway(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return highway.Hash(highway.Lanes{}, k) })
+}
 
 var crc64table = crc64.MakeTable(crc64.ECMA)
-var h64crc64 = func(k []byte) uint64 { return crc64.Checksum(k, crc64table) }
 
-func Benchmark64CRC64(b *testing.B) { benchmarkHash64(b, "crc64", h64crc64) }
-
-var h64sip13hash = func(k []byte) uint64 { return sip13.Sum64(0, 0, k) }
-
-func Benchmark64Sip13Hash(b *testing.B) { benchmarkHash64(b, "Sip13", h64sip13hash) }
-
-var h64fnva = func(k []byte) uint64 {
-	h := fnv.New64a()
-	h.Write(k)
-	return h.Sum64()
+func Benchmark64bitCRC64(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return crc64.Checksum(k, crc64table) })
 }
 
-func Benchmark64FNV1A(b *testing.B) { benchmarkHash64(b, "fnv1a", h64fnva) }
-
-var h64fnv = func(k []byte) uint64 {
-	h := fnv.New64()
-	h.Write(k)
-	return h.Sum64()
+func Benchmark64bitSip13Hash(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return sip13.Sum64(0, 0, k) })
 }
 
-func Benchmark64FNV1(b *testing.B) { benchmarkHash64(b, "fnv1", h64fnv) }
+func Benchmark64bitFNV1A(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 {
+		h := fnv.New64a()
+		h.Write(k)
+		return h.Sum64()
+	})
+}
 
-var ht1ha = func(k []byte) uint64 { return t1ha.Sum64(k, 0) }
+func Benchmark64bitFNV1(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 {
+		h := fnv.New64()
+		h.Write(k)
+		return h.Sum64()
+	})
+}
 
-func Benchmark64T1ha(b *testing.B) { benchmarkHash64(b, "T1ha", ht1ha) }
+func Benchmark64bitT1ha(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return t1ha.Sum64(k, 0) })
+}
 
 var zaphodSeed zaphod64.State
-var hzaphod64 = func(k []byte) uint64 { return uint64(zaphod64.HashWithState(&zaphodSeed, k, uint64(len(k)))) }
 
-func Benchmark64Zaphod64(b *testing.B) { benchmarkHash64(b, "Zaphod64", hzaphod64) }
+func Benchmark64bitZaphod64(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return uint64(zaphod64.HashWithState(&zaphodSeed, k, uint64(len(k)))) })
+}
 
 var stadtxState stadtx.State
-var hstadtx = func(k []byte) uint64 { return stadtx.Hash(&stadtxState, k) }
 
-func Benchmark64Stadtx(b *testing.B) { benchmarkHash64(b, "Stadtx", hstadtx) }
+func Benchmark64bitStadtx(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return stadtx.Hash(&stadtxState, k) })
+}
 
-var htsip = func(k []byte) uint64 { return tsip.HashASM(0, 0, k) }
+func Benchmark64bitTsip(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return tsip.HashASM(0, 0, k) })
+}
 
-func Benchmark64Tsip(b *testing.B) { benchmarkHash64(b, "Tsip", htsip) }
+func Benchmark32bitFarmHash(b *testing.B) { benchmarkHash(b, farmhash.Hash32) }
+func Benchmark64bitFarmHash(b *testing.B) { benchmarkHash(b, farmhash.Hash64) }
 
-var h64farmhash = func(k []byte) uint64 { return farmhash.Hash64(k) }
-
-func Benchmark64FarmHash(b *testing.B) { benchmarkHash64(b, "Farmhash", h64farmhash) }
+func Benchmark256bitSHA256(b *testing.B) { benchmarkHash(b, sha256.Sum256) }
 
 var key32 []byte = []byte("18086354421675971832404208891150")
 
-var h64highwayhash = func(k []byte) uint64 { return highwayhash.Sum64(k, key32) }
+func Benchmark64bitHighwayhash(b *testing.B) {
+	benchmarkHash(b, func(k []byte) uint64 { return highwayhash.Sum64(k, key32) })
+}
 
-func Benchmark64highwayhash(b *testing.B) { benchmarkHash64(b, "highwayhash", h64highwayhash) }
+func Benchmark128bitHighwayhash(b *testing.B) {
+	benchmarkHash(b, func(k []byte) [16]byte { return highwayhash.Sum128(k, key32) })
+}
 
-var h128highwayhash = func(k []byte) [16]byte { return highwayhash.Sum128(k, key32) }
+func Benchmark256bitHighwayhash(b *testing.B) {
+	benchmarkHash(b, func(k []byte) [32]byte { return highwayhash.Sum(k, key32) })
+}
 
-func Benchmark128highwayhash(b *testing.B) { benchmarkHash128(b, "highwayhash", h128highwayhash) }
-
-var h256highwayhash = func(k []byte) [32]byte { return highwayhash.Sum(k, key32) }
-
-func Benchmark256highwayhash(b *testing.B) { benchmarkHash256(b, "highwayhash", h256highwayhash) }
+func Benchmark256bitBlake2b(b *testing.B) { benchmarkHash(b, blake2b.Sum256) }
+func Benchmark384bitBlake2b(b *testing.B) { benchmarkHash(b, blake2b.Sum384) }
+func Benchmark512bitBlake2b(b *testing.B) { benchmarkHash(b, blake2b.Sum512) }
 
 var (
 	key0, key1 uint64
@@ -270,10 +252,30 @@ var total32 uint32
 var total64 uint64
 var total128 [16]byte
 var total256 [32]byte
+var total384 [48]byte
+var total512 [64]byte
 
-func benchmarkHash32(b *testing.B, str string, h func([]byte) uint32) {
+func benchmarkHash(b *testing.B, h interface{}) {
 	for _, n := range sizes {
-		b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash32n(b, int64(n), h) })
+		switch v := h.(type) {
+		case func([]byte) uint32:
+			b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash32n(b, int64(n), h.(func([]byte) uint32)) })
+		case func([]byte) uint64:
+			b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash64n(b, int64(n), h.(func([]byte) uint64)) })
+		case func([]byte) [16]byte:
+			b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash128n(b, int64(n), h.(func([]byte) [16]byte)) })
+		case func([]byte) [32]byte:
+			b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash256n(b, int64(n), h.(func([]byte) [32]byte)) })
+		case func([]byte) [48]byte:
+			b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash384n(b, int64(n), h.(func([]byte) [48]byte)) })
+		case func([]byte) [64]byte:
+			b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash512n(b, int64(n), h.(func([]byte) [64]byte)) })
+		case func([]byte) (uint64, uint64):
+			b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash128_2n(b, int64(n), h.(func([]byte) (uint64, uint64))) })
+		default:
+			fmt.Printf("I don't know about type %T!\n", v)
+		}
+
 	}
 }
 
@@ -284,12 +286,6 @@ func benchmarkHash32n(b *testing.B, size int64, h func([]byte) uint32) {
 	}
 }
 
-func benchmarkHash64(b *testing.B, str string, h func([]byte) uint64) {
-	for _, n := range sizes {
-		b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash64n(b, int64(n), h) })
-	}
-}
-
 func benchmarkHash64n(b *testing.B, size int64, h func([]byte) uint64) {
 	b.SetBytes(size)
 	for i := 0; i < b.N; i++ {
@@ -297,28 +293,36 @@ func benchmarkHash64n(b *testing.B, size int64, h func([]byte) uint64) {
 	}
 }
 
-func benchmarkHash128(b *testing.B, str string, h func([]byte) [16]byte) {
-	for _, n := range sizes {
-		b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash128n(b, int64(n), h) })
-	}
-}
-
 func benchmarkHash128n(b *testing.B, size int64, h func([]byte) [16]byte) {
 	b.SetBytes(size)
 	for i := 0; i < b.N; i++ {
-		total128 += h(buf[:size])
+		total128 = h(buf[:size])
 	}
 }
-
-func benchmarkHash256(b *testing.B, str string, h func([]byte) [32]byte) {
-	for _, n := range sizes {
-		b.Run(strconv.Itoa(n), func(b *testing.B) { benchmarkHash256n(b, int64(n), h) })
-	}
-}
-
-func benchmarkHash128n(b *testing.B, size int64, h func([]byte) [32]byte) {
+func benchmarkHash128_2n(b *testing.B, size int64, h func([]byte) (uint64, uint64)) {
 	b.SetBytes(size)
 	for i := 0; i < b.N; i++ {
-		total256 += h(buf[:size])
+		total64, total64 = h(buf[:size])
+	}
+}
+
+func benchmarkHash256n(b *testing.B, size int64, h func([]byte) [32]byte) {
+	b.SetBytes(size)
+	for i := 0; i < b.N; i++ {
+		total256 = h(buf[:size])
+	}
+}
+
+func benchmarkHash384n(b *testing.B, size int64, h func([]byte) [48]byte) {
+	b.SetBytes(size)
+	for i := 0; i < b.N; i++ {
+		total384 = h(buf[:size])
+	}
+}
+
+func benchmarkHash512n(b *testing.B, size int64, h func([]byte) [64]byte) {
+	b.SetBytes(size)
+	for i := 0; i < b.N; i++ {
+		total512 = h(buf[:size])
 	}
 }
